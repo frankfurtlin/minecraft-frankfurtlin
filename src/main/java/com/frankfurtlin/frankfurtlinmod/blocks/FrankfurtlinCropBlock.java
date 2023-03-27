2febf7d7362af9a593fa2cd7f33fe2972060d0a6
Frankfurtlin <linshujing2018@gmail.com>
2023-03-27 08:40:08 +0800

    feat: add strawberry(get from grass)

---
+++ src/main/java/com/frankfurtlin/frankfurtlinmod/blocks/FrankfurtlinCropBlock.java
@@ -0,0 +1,48 @@
+package com.frankfurtlin.frankfurtlinmod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+
+/**
+ * 作物方块
+ */
+public class FrankfurtlinCropBlock extends CropBlock {
+
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 5.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 7.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 10.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 12.0, 16.0)};
+
+    private final Item seed;
+
+    public FrankfurtlinCropBlock(Item seed, Settings settings) {
+        super(settings);
+        this.seed = seed;
+    }
+
+
+    @Override
+    protected ItemConvertible getSeedsItem() {
+        return seed;
+    }
+
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[state.get(this.getAgeProperty())];
+    }
+
+}
