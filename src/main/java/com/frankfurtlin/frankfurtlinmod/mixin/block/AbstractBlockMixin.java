900c2f36692792464bbb14a5d3e90673d057b9cb
Frankfurtlin <linshujing2018@gmail.com>
2023-12-01 20:16:49 +0800

    feat: remove flower offset

---
+++ src/main/java/com/frankfurtlin/frankfurtlinmod/mixin/block/AbstractBlockMixin.java
@@ -0,0 +1,29 @@
+package com.frankfurtlin.frankfurtlinmod.mixin.block;
+
+import net.minecraft.block.AbstractBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+/**
+ * 消除花放置的偏移量
+ */
+@Mixin(AbstractBlock.Settings.class)
+public abstract class AbstractBlockMixin {
+    @Accessor
+    abstract void setOffsetter(Optional<AbstractBlock.Offsetter> offsetter);
+
+    @Inject(at = @At("HEAD"), method = "offset", cancellable = true)
+    public void offset(AbstractBlock.OffsetType offsetType, CallbackInfoReturnable<AbstractBlock.Settings> cir){
+
+        this.setOffsetter(Optional.empty());
+        cir.setReturnValue((AbstractBlock.Settings)(Object)this);
+        cir.cancel();
+
+    }
+
+}
